09eaa2a8304ec7aebaf5ec2945a3daa81ebb0104
walquened <timidia3@gmail.com>
2022-06-19 09:37:34 -0300

    Adcionando os Métodos Gets e Sets .

--- ProjetoWebJava/src/Front/Usuario.java
+++ ProjetoWebJava/src/Front/Usuario.java
@@ -1,5 +1,18 @@
 package Front;
 //Class Uruario Principal
 public class Usuario {
-
+private String Cod;
+private String Nome;
+public String getCod() {
+	return Cod;
+}
+public void setCod(String cod) {
+	Cod = cod;
+}
+public String getNome() {
+	return Nome;
+}
+public void setNome(String nome) {
+	Nome = nome;
+}
 }
